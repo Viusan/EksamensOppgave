746a6654a6928cee382203a818f1cd3acd07e02d
ViusanSantos <viusansantos@gmail.com>
2023-03-21 13:17:56 +0100

    asda

--- script.js
+++ script.js
@@ -1,40 +1,59 @@
+var buttonField = document.getElementById("buttonField");
+var buttons = document.getElementsByClassName("hytteButton");
+
 let hytte = [
   {
     hytteName: "Gjendesheim",
     naerHytter: ["Glitterheim", "Memurubu"],
-    distanse: [22, 14]
+    distance: [22, 14]
   },
   {
     hytteName: "Glitterheim",
     naerHytter: ["Gjendesheim", "Memurubu", "Spiterstulen"],
-    distanse: [22, 18, 17]
+    distance: [22, 18, 17]
   },
   {
     hytteName: "Memurubu",
     naerHytter: ["Gjendesheim", "Glitterheim", "Gjendebu"],
-    distanse: [14, 18, 10]
+    distance: [14, 18, 10]
   },
   {
     hytteName: "Gjendebu",
     naerHytter: ["Memurubu", "Leirvassbu", "Spiterstulen", "Olavsbu"],
-    distanse: [10, 19, 24, 16]
+    distance: [10, 19, 24, 16]
   },
   {
     hytteName: "Leirvassbu",
     naerHytter: ["Gjendebu", "Spiterstulen", "Olavsbu"],
-    distanse: [19, 15, 11]
+    distance: [19, 15, 11]
   },
   {
     hytteName: "Spiterstulen",
     naerHytter: ["Glitterheim", "Gjendebu", "Leirvassbu"],
-    distanse: [17, 24, 15]
+    distance: [17, 24, 15]
   },
   {
     hytteName: "Olavsbu",
     naerHytter: ["Gjendebu", "Leirvassbu"],
-    distanse: [16, 11]
+    distance: [16, 11]
   },
 ]
 
-console.log(hytte[0].naerHytter[0]);
-console.log(hytte[0].distanse[0]);
+function createButton(name){
+  var buttonEl = document.createElement("button");
+  buttonEl.innerHTML = name;
+  buttonEl.className = "hytteButton";
+  buttonField.appendChild(buttonEl);
+}
+
+for(var i = 0; i < hytte.length; i++){
+  createButton(hytte[i].hytteName);
+}
+
+for(var i = 0; i < buttons; i++){
+  buttons[i].setAttribute("click", findPath);
+}
+
+function findPath(e){
+  console.log(e.target.value);
+}
